6161b863d2ee4b5e4640d2eda6eed2a904d2594d
Musa98 <www.musik2014@mail.ru>
2020-10-21 22:31:25 +0300

    init: create reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+  let StringNumber = n + '';
+  let ReverseStringNumber =  StringNumber.split('').reverse().join('');
+  return ReverseStringNumber.endsWith('-') ? ReverseStringNumber.replace('-','') : ReverseStringNumber; 
 }
